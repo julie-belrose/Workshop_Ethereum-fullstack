811a155d47161172db6d8185e158863436fa4489
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2024-11-15 15:15:36 +0100

    add config hardhat

--- react-dapp/hardhat.config.cjs
+++ react-dapp/hardhat.config.cjs
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity:" 0.8.27",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  }
 };
